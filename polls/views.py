f625372e3a336d656400529e9d05b2cf46ae4275
Jonathan Ochaya <wangolojowel@gmail.com>
2023-02-26 16:48:14 +0300

    Added index and detail view for showing list of all available questions and viewing individual question details - added app template directory and created simple templates to display the question details - added url to view mapping and also registered the Choice and Question model to the django admin interface

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse('Hello, World. You\'re at the polls index.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {
+        'latest_question_list': latest_question_list
+    })
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {
+        'question': question
+    })
+
+def results(requst, question_id):
+    pass
+
+def vote(request, question_id):
+    pass
