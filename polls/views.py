46370f4b6f3a46ea580a805d125af080028caa4f
Jonathan Ochaya <wangolojowel@gmail.com>
2023-02-26 23:16:30 +0300

    Added ability to vote for choice on question - added view logic to save choices avoiding race conditions using django's 'F' function

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -14,8 +17,24 @@ def detail(request, question_id):
         'question': question
     })
 
-def results(requst, question_id):
-    pass
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {
+        'question': question
+    })
+
 
 def vote(request, question_id):
-    pass
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You didn\'t select a choice'
+        })
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
